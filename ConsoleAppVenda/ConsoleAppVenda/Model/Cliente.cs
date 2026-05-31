4ab9a08316f65c2f69dd25be58453698b9f057db
Victor Rodrigues <victorfaculdadeunimar@gmail.com>
2026-05-30 22:58:52 -0300

    criando Models Cliente e Divida

---
+++ ConsoleAppVenda/ConsoleAppVenda/Model/Cliente.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VendinhaPlena.Models;
+
+public class Cliente
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Nome obrigatório")]
+    [StringLength(100)]
+    public string NomeCompleto { get; set; }
+
+    [Required(ErrorMessage = "CPF obrigatório")]
+    [StringLength(11)]
+    public string CPF { get; set; }
+
+    [Required]
+    public DateTime DataNascimento { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+
+    public int Idade
+    {
+        get
+        {
+            var hoje = DateTime.Today;
+
+            var idade = hoje.Year - DataNascimento.Year;
+
+            if (DataNascimento.Date > hoje.AddYears(-idade))
+            {
+                idade--;
+            }
+
+            return idade;
+        }
+    }
+
+    public virtual void PrintDados()
+    {
+        Console.WriteLine($"Nome: {NomeCompleto}");
+        Console.WriteLine($"CPF: {CPF}");
+        Console.WriteLine($"Nascimento: {DataNascimento:dd/MM/yyyy}");
+        Console.WriteLine($"Idade: {Idade}");
+        Console.WriteLine($"Email: {Email}");
+    }
+}
---
+++ ConsoleAppVenda/ConsoleAppVenda/Model/Divida.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VendinhaPlena.Enums;
+
+namespace VendinhaPlena.Models;
+
+public class Divida
+{
+    public int Id { get; set; }
+
+    [Required]
+    public Cliente Cliente { get; set; }
+
+    [Range(0.01, double.MaxValue)]
+    public decimal Valor { get; set; }
+
+    public SituacaoDivida Situacao { get; set; }
+
+    public DateTime DataCriacao { get; private set; }
+
+    public DateTime? DataPagamento { get; set; }
+
+    public Divida()
+    {
+        DataCriacao = DateTime.Now;
+        Situacao = SituacaoDivida.Aberta;
+    }
+}
